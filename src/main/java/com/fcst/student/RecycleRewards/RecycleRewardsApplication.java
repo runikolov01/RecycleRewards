076c3ef53674bbdc0ff1fe29dcec0d09ddc87d92
Rumen Nikolov <rumenhco2001@abv.bg>
2024-03-12 15:11:44 +0200

    config files

---
+++ src/main/java/com/fcst/student/RecycleRewards/RecycleRewardsApplication.java
@@ -0,0 +1,13 @@
+package com.fcst.student.RecycleRewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecycleRewardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecycleRewardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fcst/student/RecycleRewards/RecycleRewardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcst.student.RecycleRewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecycleRewardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
