32b5327bd613d8a64d17ecaf38d55623d5087a47
Rumen Nikolov <rumenhco2001@abv.bg>
2024-04-17 15:44:34 +0300

    Create Machine entity

---
+++ src/main/java/com/fcst/student/RecycleRewards/model/Machine.java
@@ -0,0 +1,64 @@
+package com.fcst.student.RecycleRewards.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "machines")
+public class Machine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "location_name", nullable = false)
+    private String locationName;
+
+    @OneToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @Column(nullable = false)
+    private String barcode;
+
+    public Machine() {
+
+    }
+
+    public Machine(Long id, String locationName, Address address, String barcode) {
+        this.id = id;
+        this.locationName = locationName;
+        this.address = address;
+        this.barcode = barcode;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+}
