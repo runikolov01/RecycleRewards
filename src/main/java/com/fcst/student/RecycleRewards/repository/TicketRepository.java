e8c1ac1d2ab1ab097e5219c373aa9ad8b6599297
Rumen Nikolov <rumenhco2001@abv.bg>
2024-04-30 22:28:17 +0300

    Add @Repository annotation

--- src/main/java/com/fcst/student/RecycleRewards/repository/TicketRepository.java
+++ src/main/java/com/fcst/student/RecycleRewards/repository/TicketRepository.java
@@ -2,7 +2,9 @@ package com.fcst.student.RecycleRewards.repository;
 
 import com.fcst.student.RecycleRewards.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     boolean existsByNumber(String number);
 
