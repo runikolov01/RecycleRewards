6c5c8d97de88c6b1280122c2b84c0fb89af6b92a
Rumen Nikolov <rumenhco2001@abv.bg>
2024-05-01 23:53:24 +0300

    homepage added

---
+++ src/main/java/com/fcst/student/RecycleRewards/web/HomeController.java
@@ -0,0 +1,12 @@
+package com.fcst.student.RecycleRewards.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
