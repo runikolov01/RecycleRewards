0161955369aad36c2e6f5c346e0f024e9309ffbf
Rumen Nikolov <rumenhco2001@abv.bg>
2024-05-02 18:44:07 +0300

    Update PrizeRepository

--- src/main/java/com/fcst/student/RecycleRewards/repository/PrizeRepository.java
+++ src/main/java/com/fcst/student/RecycleRewards/repository/PrizeRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PrizeRepository extends JpaRepository<Long, Prize> {
+public interface PrizeRepository extends JpaRepository<Prize, Long> {
 }
