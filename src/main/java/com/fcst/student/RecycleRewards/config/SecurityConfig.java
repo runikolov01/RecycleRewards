d19721040005cf9205bb07dba53c519127f24357
Rumen Nikolov <rumenhco2001@abv.bg>
2024-05-02 12:03:40 +0300

    Update SpringSecurity

--- src/main/java/com/fcst/student/RecycleRewards/config/SecurityConfig.java
+++ src/main/java/com/fcst/student/RecycleRewards/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.fcst.student.RecycleRewards.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
