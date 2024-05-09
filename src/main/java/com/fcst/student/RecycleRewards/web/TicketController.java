e0d67073a911fee4930935a6ff6d970bff6922d8
Rumen Nikolov <rumenhco2001@abv.bg>
2024-05-09 17:49:10 +0300

    combining controllers

--- src/main/java/com/fcst/student/RecycleRewards/web/TicketController.java
+++ src/main/java/com/fcst/student/RecycleRewards/web/TicketController.java
@@ -3,8 +3,10 @@ package com.fcst.student.RecycleRewards.web;
 import com.fcst.student.RecycleRewards.model.Ticket;
 import com.fcst.student.RecycleRewards.model.User;
 import com.fcst.student.RecycleRewards.repository.TicketRepository;
+import com.fcst.student.RecycleRewards.service.TicketService;
 import com.fcst.student.RecycleRewards.service.UserService;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
 @Controller
@@ -23,11 +26,98 @@ public class TicketController {
 
     private final TicketRepository ticketRepository;
     private final UserService userService;
+    @Autowired
+    private TicketService ticketService;
 
     public TicketController(TicketRepository ticketRepository, UserService userService) {
         this.ticketRepository = ticketRepository;
         this.userService = userService;
     }
+
+    private void setAttributesAndModel(HttpSession session, Model model, Ticket ticket, Integer bottlesCount) {
+        String issuedOnFormatted = ticket.getIssued().format(DateTimeFormatter.ofPattern("dd/MM/yy HH:mm"));
+        String expirationFormatted = ticket.getActiveUntil().format(DateTimeFormatter.ofPattern("dd/MM/yy HH:mm"));
+
+        session.setAttribute("ticketNumber", ticket.getNumber());
+        session.setAttribute("issuedOnFormatted", issuedOnFormatted);
+        session.setAttribute("expirationFormatted", expirationFormatted);
+
+        model.addAttribute("bottlesCount", bottlesCount);
+        model.addAttribute("ticketNumber", ticket.getNumber());
+        model.addAttribute("issuedOnFormatted", issuedOnFormatted);
+        model.addAttribute("expirationFormatted", expirationFormatted);
+    }
+
+    @GetMapping("/index")
+    public String openHomePage() {
+        return "index";
+    }
+
+    @GetMapping("/start")
+    public String openStartPage(HttpSession session, Model model) {
+        Integer bottlesCount = (Integer) session.getAttribute("bottlesCount");
+        bottlesCount = ticketService.getDefaultBottlesCount(bottlesCount);
+
+        model.addAttribute("bottlesCount", bottlesCount);
+        return "start";
+    }
+
+    @GetMapping("/print")
+    public String openPrintPage(HttpSession session, Model model) {
+        Integer bottlesCount = (Integer) session.getAttribute("bottlesCount");
+        String ticketNumber = (String) session.getAttribute("ticketNumber");
+        bottlesCount = ticketService.getDefaultBottlesCount(bottlesCount);
+
+        if (ticketNumber == null) {
+            ticketNumber = ticketService.generateUniqueTicketNumber(); // Generate a new ticket number if not already generated
+            session.setAttribute("ticketNumber", ticketNumber); // Store it in the session
+        }
+
+        Ticket ticket = ticketService.createTicket(bottlesCount, ticketNumber);
+        setAttributesAndModel(session, model, ticket, bottlesCount);
+
+        return "print";
+    }
+
+    @GetMapping("/home")
+    public String home(Model model, HttpSession session) {
+        // Check if the user is logged in
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        boolean loggedIn = authentication != null && authentication.isAuthenticated();
+
+        // Add the loggedIn attribute to the model
+        model.addAttribute("loggedIn", loggedIn);
+
+        Long userId = (Long) session.getAttribute("userId");
+        if (userId != null) {
+            User user = userService.getUserById(userId);
+            if (user != null) {
+                model.addAttribute("loggedUser", user);
+
+                // Fetch totalPoints for the logged-in user and add it to the model
+                Integer totalPoints = user.getTotalPoints();
+                model.addAttribute("totalPoints", totalPoints);
+            }
+            System.out.println(user.getRole());
+        }
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model, HttpSession session) {
+        Long userId = (Long) session.getAttribute("userId");
+        if (userId != null) {
+            // Fetch user details from the database using the user ID
+            User user = userService.getUserById(userId);
+            if (user != null) {
+                model.addAttribute("loggedUser", user);
+                Integer totalPoints = user.getTotalPoints();
+                model.addAttribute("totalPoints", totalPoints);
+            }
+        }
+        return "about";
+    }
+
 
     @GetMapping("/registerTicket")
     public String registerTicket(Model model, HttpSession session) {
@@ -54,6 +144,36 @@ public class TicketController {
             }
         }
         return "registerTicket";
+    }
+
+    @PostMapping("/start")
+    public String addBottle(HttpSession session) {
+        Integer bottlesCount = (Integer) session.getAttribute("bottlesCount");
+        bottlesCount = ticketService.getDefaultBottlesCount(bottlesCount);
+        bottlesCount++;
+
+        session.setAttribute("bottlesCount", bottlesCount);
+
+        return "redirect:/start";
+    }
+
+    @PostMapping("/print")
+    public String exitSession(HttpSession session, Model model) {
+        Integer bottlesCount = (Integer) session.getAttribute("bottlesCount");
+        bottlesCount = ticketService.getDefaultBottlesCount(bottlesCount);
+        String ticketNumber = (String) session.getAttribute("ticketNumber");
+
+        Ticket ticket = ticketService.createTicket(bottlesCount, ticketNumber);
+        setAttributesAndModel(session, model, ticket, bottlesCount);
+
+        ticketService.saveTicket(ticket);
+        session.setAttribute("bottlesCount", 0);
+        session.invalidate();
+
+        // Passing the alert message as a parameter
+        model.addAttribute("alertMessage", "Printing...");
+
+        return "print_with_alert";
     }
 
     @PostMapping("/registerTicket")
