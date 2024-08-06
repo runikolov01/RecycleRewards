1a644b271411bdf160ad6e26cd634d7e7953c579
Rumen Nikolov <rumenhco2001@abv.bg>
2024-08-06 21:56:57 +0300

    Revert "deleting the REST service"
    This reverts commit deff4edb1d84a43226e9f9b578cdeb30a073c00e.

---
+++ RecycledBottlesReports/src/test/java/com/fcst/student/RecycledBottlesReports/RecycledBottlesReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcst.student.RecycledBottlesReports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecycledBottlesReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
